9e4b91caf76f9590e295caa302980041a8f0055a
nikitsrj <swarajnikit@gmail.com>
2020-02-25 15:48:52 +0800

    fixing terraform syntax issue

--- ansible.tf
+++ ansible.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "ansible" {
-  depends_on = [aws_instance.web]
+  depends_on = ["aws_instance.web"]
   provisioner "local-exec" {
   	command = "sed -i \"s/SERVER1_IP/${aws_instance.web.public_ip}/g\" ansible/hosts"
   }
--- route53.tf
+++ route53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "webgoat" {
-  depends_on = [null_resource.ansible]
+  depends_on = ["null_resource.ansible"]
   zone_id = "Z2JR0FEN9RROGY"
   name    = "webgoat.nttdemo.net"
   type    = "A"
