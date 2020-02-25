747ab83122998ec9544bc9aa423527377269614f
nttsgdevops <61444624+nttsgdevops@users.noreply.github.com>
2020-02-25 20:00:20 +0800

    Update ec2.tf

--- sgx-iac/ec2.tf
+++ sgx-iac/ec2.tf
@@ -54,8 +54,7 @@ resource "aws_instance" "web" {
   vpc_security_group_ids  = ["${aws_security_group.app-sg.id}"]
   tags = {
     Name = "devsecops"
-    ttl = "20"
-    owner = "nikit"
+
   }
   
 }
