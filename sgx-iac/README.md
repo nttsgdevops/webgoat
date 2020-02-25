a234eccefff88f6ce0c06596782457124650485e
nikitsrj <swarajnikit@gmail.com>
2020-02-25 14:54:03 +0800

    first push with sentinel modification

---
+++ sgx-iac/README.md
@@ -0,0 +1,2 @@
+# terraform-sentinel-aws
+This repository includes sample terraform code to spin ec2 instance and with validation of sentinel policy as a code
---
+++ sgx-iac/ansible.tf
@@ -0,0 +1,14 @@
+resource "null_resource" "ansible" {
+  depends_on = ["aws_instance.web"]
+  provisioner "local-exec" {
+  	command = "sed -i \"s/SERVER1_IP/${aws_instance.web.public_ip}/g\" ansible/hosts"
+  }
+  provisioner "local-exec" {
+    command = "sleep 10"
+  }
+  provisioner "local-exec" {
+    command = "sleep 20 && cd ansible && ansible-playbook -i hosts playbook.yaml -vvv"
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
